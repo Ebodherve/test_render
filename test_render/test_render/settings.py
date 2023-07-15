d8cbfcb3329dac13128947958bb45cc4801cd381
Ebodherve <tsangaebodepython@gmail.com>
2023-07-15 20:37:25 +0100

    Modification de la variable debug

--- test_render/test_render/settings.py
+++ test_render/test_render/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+_77-#iexj*5pfgut_4#7%um(!297m(5t$g++86#0nit_e*sld'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
